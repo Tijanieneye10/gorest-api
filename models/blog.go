7ab325b60f47a05af32c44952e797f6ebd138b45
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-12 11:02:23 +0100

    wip

---
+++ models/blog.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Blog struct {
+	ID        uint64    `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	UserID    uint64    `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
