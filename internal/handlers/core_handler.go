2ab60a8d0793c61848ce1a2a1644a23a2aa4ea2b
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-14 00:16:38 +0100

    wip

--- internal/handlers/core_handler.go
+++ internal/handlers/core_handler.go
@@ -1,6 +1,14 @@
 package handlers
 
+import (
+	"database/sql"
+
+	"github.com/tijanieneye10/restapi/internal/store"
+)
+
 type Handler struct {
+	DB      *sql.DB
+	Queries *store.Queries
 }
 
 func NewHandler() *Handler {
--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -2,20 +2,35 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/tijanieneye10/restapi/internal/dtos"
+	"github.com/tijanieneye10/restapi/internal/store"
 	"github.com/tijanieneye10/restapi/internal/utils"
 )
 
 func (h Handler) CreateUser() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 		var req dtos.UserDto
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
 			utils.ErrorResponse(w, "invalid request", err.Error(), http.StatusBadRequest)
 		}
 
-		utils.SuccessResponse(w, "User Created successfully", req, http.StatusCreated)
+		password, err := utils.HashPassword(req.Password)
+
+		user, err := h.Queries.CreateUser(ctx, store.CreateUserParams{
+			Username: req.Username,
+			Password: password,
+			Email:    req.Email,
+		})
+
+		if err != nil {
+			log.Fatalf("error %v", err.Error())
+		}
+
+		utils.SuccessResponse(w, "User Created successfully", user, http.StatusCreated)
 	}
 }
